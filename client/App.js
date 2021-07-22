eeb75e8cd3d7b7c9db6ce012b4197ebf7e624d5b
Galuh Alifani <galuh.adika@gmail.com>
2021-07-22 20:27:18 +0700

    redux, redux thunk, store, app.js client, router server

--- client/App.js
+++ client/App.js
@@ -1,13 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Provider } from 'react-redux'
+import store from "./store";
+import Login from './screens/Login.js'
+import Register from './screens/Register.js'
+import Home from './screens/Home.js'
+import AddExpense from './screens/AddExpense.js'
+import EditExpense from './screens/EditExpense.js'
+import MyProfile from './screens/MyProfile.js'
+import ExpenseReport from './screens/myProfile.js'
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <View style={styles.container}>
+
+          {/* LOGIN & REGISTER */}
+          <Stack.Screen name="Login" component={Login}/>
+          <Stack.Screen name="Register" component={Register}/>
+
+          {/* SUMMARY PAGE */}
+          <Stack.Screen name="Home" component={Home}/>
+
+          {/* EDIT EXPENSE */}
+          <Stack.Screen name="AddExpense" component={AddExpense} />
+          <Stack.Screen name="EditExpense" component={EditExpense} />
+
+          {/* PROFILE & ANALYTICS EXPENSE */}
+          <Stack.Screen name="MyProfile" component={MyProfile}/>
+          <Stack.Screen name="ExpenseReport" component={ExpenseReport}/>
+
+        <StatusBar style="auto" />
+      </View>
+
+    </Stack.Navigator>
+    </NavigationContainer>
+    </Provider>
   );
 }
 
