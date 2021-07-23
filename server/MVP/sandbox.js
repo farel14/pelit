b61540087b6c7370b8acab4a80aa436705d1988d
farel14 <72905415+farel14@users.noreply.github.com>
2021-07-23 18:19:58 +0700

    add resseract

---
+++ server/MVP/sandbox.js
@@ -0,0 +1,4 @@
+const tesseract = require('./tesseract')
+
+// console.log(typeof tesseract)
+console.log(typeof tesseract('https://image.freepik.com/free-vector/realistic-receipt-template_23-2147938550.jpg'))
