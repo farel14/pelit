8ba5ab1a22a88626e05e896935b9d669961eeb0e
Galuh Alifani <galuh.adika@gmail.com>
2021-07-23 16:21:33 +0700

    server target, testing TDD target

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,11 +8,11 @@ let target = require('./target.js')
 let badge = require('./badge')
 let achievement = require('./achievement')
 
-router.use('/', home); // Login dan Register masuk ke route ini
-router.use('/user', user); // Login dan Register masuk ke route ini
-router.use('/transactions', transactions);
+// router.use('/', home);
+// router.use('/user', user);
+// router.use('/transactions', transactions);
 router.use('/target', target);
-router.use('/badge', badge);
-router.use('/achievement', achievement)
+// router.use('/badge', badge);
+// router.use('/achievement', achievement)
 
 module.exports = router
