3f6067b202ce5e8b9eddf6ba5d9e13d0d40afcf9
Galuh Alifani <galuh.adika@gmail.com>
2021-07-26 19:31:28 +0700

    route fix

--- routes/transactions.js
+++ routes/transactions.js
@@ -16,19 +16,8 @@ router.get("/date/:UserId/:month", Transaction.getAllGroupedByDate);
 router.get("/expense/:TransactionId", Transaction.getByTransactionId);
 
 // for analytics
-<<<<<<< HEAD
 router.get('/between/:startDate/:endDate/:UserId/:type', Transaction.getBetweenTwoDatesByType)
 router.get('/between/:startDate/:endDate/:UserId', Transaction.getBetweenTwoDates)
-=======
-router.get(
-  "/between/:startDate/:endDate/:UserId/:type",
-  Transaction.getBetweenTwoDatesByType
-);
-router.get(
-  "/between/:startDate/:endDate/:UserId",
-  Transaction.getBetweenTwoDates
-);
->>>>>>> 892175bc44b3dbef85619df54928102010bd9874
 
 // for summary page
 router.get("/:UserId/:type", Transaction.getByType); // by income / by expense for each userId in a specific month
