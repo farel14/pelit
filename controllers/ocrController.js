0a28038989c09dbed77332ee57e23635c51d46ee
farel14 <72905415+farel14@users.noreply.github.com>
2021-07-26 17:59:32 +0700

    backend ocr worked

--- controllers/ocrController.js
+++ controllers/ocrController.js
@@ -2,13 +2,18 @@ const tesseract = require('../OCR')
 
 class OCRController {
     static postOcr(req, res) {
-        const { imageUrl } = req.body
-        console.log(req.body)
-
+        const imageUrl = req.file.buffer
+        console.log('masuk controller', imageUrl)
+        // const imageUrl = req.urlImage
+        // const { imageUrl } = req.body
+        // console.log(req.body)
+        // console.log('da IMAGE',imageUrl)
+        
         if (!imageUrl) return res.status(400).json({message: 'bad request'})
 
         tesseract(imageUrl)
             .then(dataObj => {
+                console.log('masuk tesseract', dataObj)
                 res.status(200).json(dataObj)
             })
             .catch(err => {
