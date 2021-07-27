ec1fa3ac7b46cc9d27a9a20427ee6d9d3493d130
Ahmadfaisal98 <achmad_fsl@yahoo.com>
2021-07-27 23:53:30 +0700

    increase tdd score

--- OCR/tesseract.js
+++ OCR/tesseract.js
@@ -1,20 +1,21 @@
-const { createWorker } = require('tesseract.js');
+const { createWorker } = require("tesseract.js");
 module.exports = (receiptImageUrl) => {
-    const innerFunction = (async () => {
-        try {
-            
-            const worker = createWorker();
-            await worker.load();
-            await worker.loadLanguage('eng');
-            await worker.initialize('eng');
-            const { data: { text } } = await worker.recognize(receiptImageUrl);
-            await worker.terminate();
-            
-            return text
-        } catch (error) {
-            console.error(error)   
-        }
-    })();
+  const innerFunction = (async () => {
+    try {
+      const worker = createWorker();
+      await worker.load();
+      await worker.loadLanguage("eng");
+      await worker.initialize("eng");
+      const {
+        data: { text },
+      } = await worker.recognize(receiptImageUrl);
+      await worker.terminate();
 
-    return innerFunction
-}
+      return text;
+    } catch (error) {
+      // console.error(error)
+    }
+  })();
+
+  return innerFunction;
+};
